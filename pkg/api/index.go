6196e1d9bc1c7de87d894b3ec49cf39d2cb56a49
Ka Shing <kschung@cloudian.com>
2021-04-15 18:10:46 -0700

    remove grafana version in help tab

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -303,14 +303,9 @@ func (hs *HTTPServer) getNavTree(c *models.ReqContext, hasEditPerm bool) ([]*dto
 		})
 	}
 
-	helpVersion := fmt.Sprintf(`%s v%s (%s)`, setting.ApplicationName, setting.BuildVersion, setting.BuildCommit)
-	if hs.Cfg.AnonymousHideVersion && !c.IsSignedIn {
-		helpVersion = setting.ApplicationName
-	}
-
 	navTree = append(navTree, &dtos.NavLink{
 		Text:         "Help",
-		SubTitle:     helpVersion,
+		SubTitle:     "",
 		Id:           "help",
 		Url:          "#",
 		Icon:         "question-circle",
