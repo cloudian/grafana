90a5f26881e8fe98eef4377d8246cc72b2aea008
Jaspal Chauhan <jchauhan@cloudian.com>
2026-01-12 13:22:41 -0800

    Remove enterprise/cloud cards

--- public/app/features/auth-config/AuthProvidersListPage.tsx
+++ public/app/features/auth-config/AuthProvidersListPage.tsx
@@ -9,8 +9,6 @@ import { Page } from 'app/core/components/Page/Page';
 import { config } from 'app/core/config';
 import { StoreState } from 'app/types/store';
 
-import { isOpenSourceBuildOrUnlicenced } from '../admin/EnterpriseAuthFeaturesCard';
-
 import AuthDrawer from './AuthDrawer';
 import ConfigureAuthCTA from './components/ConfigureAuthCTA';
 import { ProviderCard, ProviderSAMLCard, ProviderSCIMCard } from './components/ProviderCard';
@@ -121,12 +119,6 @@ export const AuthConfigPageUnconnected = ({
                   configPath={settings.configPath}
                 />
               ))}
-            {isOpenSourceBuildOrUnlicenced() && (
-              <>
-                <ProviderSAMLCard />
-                <ProviderSCIMCard />
-              </>
-            )}
             {showDrawer && <AuthDrawer onClose={() => setShowDrawer(false)}></AuthDrawer>}
           </Grid>
         )}
