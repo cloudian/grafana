5b6deb3c31454df6aa73ff5f516ecb87e986bb84
Jaspal Chauhan <jchauhan@cloudian.com>
2026-03-23 11:46:34 -0700

    Change dashboard filter to use proper LogInsight tag

--- cloudian_loginsight.sh
+++ cloudian_loginsight.sh
@@ -7,10 +7,8 @@ echo "$SELF"
 
 # Files to not change:
 # hyperiq_guide.pdf
-# navtree.go: Keep 'HyperIQ' tag for dashboard links
 find "$PWD" -type f \
   -not -name "$SELF" \
   -not -name hyperiq_guide.pdf \
-  -not -name navtree.go \
   -print0 |
   xargs -0 sed -i 's/HyperIQ/LogInsight/g'
