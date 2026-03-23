34aa9e1c400de07875380f14fbfe8bed77a53574
Ka Shing <kschung@cloudian.com>
2026-03-23 11:32:43 -0700

    HIQ-1405   Grafana "Open Source" link goes to wrong location

--- pkg/services/licensing/oss.go
+++ pkg/services/licensing/oss.go
@@ -34,11 +34,7 @@ func (*OSSLicensingService) ContentDeliveryPrefix() string {
 }
 
 func (l *OSSLicensingService) LicenseURL(showAdminLicensingPage bool) string {
-	if showAdminLicensingPage {
-		return l.Cfg.AppSubURL + "/admin/upgrading"
-	}
-
-	return "https://grafana.com/oss/grafana?utm_source=grafana_footer"
+	return "https://github.com/cloudian/grafana"
 }
 
 func (*OSSLicensingService) EnabledFeatures() map[string]bool {
